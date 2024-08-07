7a1a3e2cfda8028a65c0e8f24fd09b0482a9802f
Shravani-CV <145748032+Shravani-CV@users.noreply.github.com>
2024-08-07 05:40:51 +0530

    Add files via upload

---
+++ python-whatsapp-bot/qr_generation.py
@@ -0,0 +1,20 @@
+import qrcode
+
+# WhatsApp chat link
+whatsapp_link = "https://wa.me/<8660778701>?text=Hello"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(whatsapp_link)
+qr.make(fit=True)
+
+# Create an image
+img = qr.make_image(fill='black', back_color='white')
+
+# Save the image
+img.save("whatsapp_qr_code.png")
